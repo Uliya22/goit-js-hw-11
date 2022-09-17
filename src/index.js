85c5f3da8a548f18c276fee2a77818255080a95a
Uliya22 <uliya.d@ukr.net>
2022-09-17 18:12:19 +0300

    виправлення зауважень

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
  const searchForm = document.querySelector('.search-form');
  const galleryBox = document.querySelector('.gallery');
  const loadMoreBtn = document.querySelector('.load-more');
- const endText = document.querySelector('.end-text')
 
  let currentPage = 1;
  let currentHits = 40;
@@ -25,30 +24,27 @@ searchForm.addEventListener('submit', onFormSubmit);
 
 async function onFormSubmit (e) {
     e.preventDefault();
-  imagesColetion = e.currentTarget.searchQuery.value;
+  imagesColetion = e.currentTarget.searchQuery.value.trim();
   
   console.log(imagesColetion);
   currentPage = 1;
   currentHits = 40;
   loadMoreBtn.classList.add('is-hidden');
-  endText.classList.add('is-hidden');
+
   clearMarkup(); 
 
         if (
             imagesColetion === '') {
         
         return}
-
+      try {
         const imageCards = await getImageCards(imagesColetion, currentPage);
-  
-
+       
         if (imageCards.totalHits > 40) {
             loadMoreBtn.classList.remove('is-hidden');
           } else {
             loadMoreBtn.classList.add('is-hidden');
           }  
-    
-    try {  
         if (imageCards.totalHits > 0) {
 
           Notify.success(`Hooray! We found ${imageCards.totalHits} images.`);
@@ -61,12 +57,14 @@ async function onFormSubmit (e) {
           }   
     
         else {
-            Notify.failure('Sorry, there are no images matching your search query. Please try again')
+            Notify.failure('Sorry, there are no images matching your search query. Please try again', {
+              clickToClose: true,
+              timeout: 5000
+            });
         }
 
-  
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
 }
 
@@ -74,9 +72,9 @@ async function onFormSubmit (e) {
 async function getImageCards  (value, page)  {
     
         const API_KEY = '29802953-73b28e5a5bd13a1bc7d407030';
-        const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true&lang=en&lang=uk&page=4&per_page=40'
+        const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true&lang=en&lang=uk&page=1&per_page=40'
         const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${value}&${OPTIONS}&page=${page}`;
-        const imageCards = (await axios.get(`${BASE_URL}`).then(response => response.data));
+        const imageCards = (await axios.get(`${BASE_URL}`)).data;
           return imageCards;
         
     };
@@ -93,16 +91,16 @@ function renderCardImage(arr) {
               </a>
               <div class="info">
                   <p class="info-item">
-                      <b>Likes: </b>${likes}
+                      <b>Likes: </b><br>${likes}
                   </p>
                   <p class="info-item">
-                      <b>Views: </b>${views}
+                      <b>Views: </b><br>${views}
                   </p>
                   <p class="info-item">
-                      <b>Comments: </b>${comments}
+                      <b>Comments: </b><br>${comments}
                   </p>
                   <p class="info-item">
-                      <b>Downloads: </b>${downloads}
+                      <b>Downloads: </b><br>${downloads}
                   </p>
               </div>
           </div>`}).join('');
@@ -134,9 +132,12 @@ async function onClickLoadMoreBtn() {
   currentHits += imageCardsArray.length;
   console.log(currentHits);
   
-  if (currentHits === response.totalHits) {
+  if (currentHits >= response.totalHits) {
     loadMoreBtn.classList.add('is-hidden');
-    endText.classList.remove('is-hidden');
+    Notify.info("We're sorry, but you've reached the end of search results.", {
+      clickToClose: true,
+      timeout: 5000,
+    });
   }
 }
 
